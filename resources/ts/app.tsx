442b3227fb059ca4d9b58962f69e58221f350dcd
takurou <takurou.bhw.useru@gmail.com>
2024-08-13 10:23:37 +0900

    tailwind設定

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
+import "../css/app.css";
 
 const App = () => {
     const title: string = "Laravel 9 Vite with TypeScript React !!";
-    return <h1>{title}</h1>;
+    return <h1 className="text-red-400">{title}</h1>;
 };
 const container = document.getElementById('app') as HTMLInputElement;
 const root = createRoot(container);
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/ts/app.tsx"],
+            input: ["resources/ts/app.tsx", "resources/css/app.css"],
             refresh: true,
         }),
         react(),
