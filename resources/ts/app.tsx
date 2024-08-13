48926e462c62e3ccdf6371d74c44ad8f35fedc0a
takurou <takurou.bhw.useru@gmail.com>
2024-08-13 10:06:34 +0900

    Vite React Typescript設定

---
+++ resources/ts/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+    const title: string = "Laravel 9 Vite with TypeScript React !!";
+    return <h1>{title}</h1>;
+};
+const container = document.getElementById('app') as HTMLInputElement;
+const root = createRoot(container);
+root.render(<App />);
