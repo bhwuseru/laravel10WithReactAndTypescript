42ceb190048d371b7f688d9f851123e0a1fb64f3
takurou <takurou.bhw.useru@gmail.com>
2024-08-13 09:11:23 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/main.tsx"],
+            refresh: true,
+        }),
+        react(),
+    ],
+    server: {
+        host: true,
+        port: 18044,
+        hmr: {
+            host: "localhost",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
+});
